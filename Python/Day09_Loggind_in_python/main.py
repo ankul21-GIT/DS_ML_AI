36cc28fba1da89feee428283a976b18c9206d697
ankul21-GIT <ankulsingh2151@gmail.com>
2025-07-23 12:28:33 +0530

    Ankul

--- Python/Day09_Loggind_in_python/main.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-def module_a_function():
-    logger = logging.getLogger(__name__)
-    logger.info('Module A function started')
-    logger.debug('This is a debug message from Module A')
-    logger.info('Module A function finished')
